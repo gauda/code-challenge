9c0d06c6fc77c72633b4cca34ddd1abf350443bd
hans <johannes@leers.eu>
2022-09-22 15:12:58 +0200

    add tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,9 +18,20 @@ RSpec.describe User, type: :model do
     expect(subject).to_not be_valid
   end
 
-  it 'is not be valid without matching password' do
+  it 'is not valid without matching password' do
     subject.password = 'someweiredpassword!!'
     subject.password_confirmation = 'someweiredpassword'
     expect(subject).to_not be_valid
+  end
+
+  it 'raises if undefined status is used' do
+    expect { subject.status = 'foo' }.to raise_error(ArgumentError)
+  end
+
+  it 'is valid if defined status is used' do
+    User.statuses.keys.prepend(nil, '').each do |status|
+      subject.status = status
+      expect(subject).to be_valid
+    end
   end
 end
